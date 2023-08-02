76919a266f58611944af3a0b83f0dcecac8a2d10
MirageM <imiragesz@gmail.com>
2023-08-01 23:04:15 -0400

    private void Search(int[] candidates, int target, int index, int IList<int> temp, IList<IList<int>> result)

--- C# Study/CombinationSum.cs
+++ C# Study/CombinationSum.cs
@@ -24,3 +24,75 @@ public class Solution {
         }
     }
 }
+
+// Try 1
+public class Solution1 {
+    public IList<IList<int>> CombinationSum(int[] candidates, int target){
+        var result = new List<IList<int>>();
+        Search(candidates, target, 0, 0, new List<int>(), result);
+        return result;
+    }
+    private void Search(int[] candidates, int target, int index, int IList<int> temp, IList<IList<int>> result)
+    {
+        if(sum == target)
+        {
+            result.Add(temp.ToArray());
+            return;
+        }
+        while(sum < target && index << candidates.Length)
+        {
+            temp.Add(candidates[index]);
+            Search(candidates, target, index, sum + candidates[index], temp, result);
+            temp.RemoveAt(temp.Count - 1);
+            index++;
+        }
+    }
+}
+
+// Try 2
+public class Solution2{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target){
+        var result = new List<IList<int>>();
+        Search(candidates, target, 0, 0, new List<int>(), result);
+        return result;
+    }
+    private void Search(int[] candidates, int target, int index, int IList<int> temp, IList<IList<int>> result)
+    {
+        if(sum == target)
+        {
+            result.Add(temp.ToArray());
+            return;
+        }
+        while(sum < target && index << candidates.Length)
+        {
+            temp.Add(candidates[index]);
+            Search(candidates, target, index, sum + candidates[index], temp, result);
+            temp.RemoveAt(temp.Count - 1);
+            index++;
+        }
+    }
+}
+
+// Try 3
+public class Solution3{
+    public IList<IList<int>> CombinationSum(int[] candidates, int target){
+        var result = new List<IList<int>>();
+        Search(candidates, target, 0, 0, new List<int>(), result);
+        return result;
+    }
+    private void Search(int[] candidates, int target, int index, int IList<int> temp, IList<IList<int>> result)
+    {
+        if (sum == target)
+        {
+            result.Add(temp.ToArray());
+            return;
+        }
+        while(sum < target && index << candidates.Length)
+        {
+            temp.Add(candidates[index]);
+            Search(candidates, target, index, sum + candidates[index], temp, result);
+            temp.RemoveAt(temp.Count - 1);
+            index++;
+        }
+    }
+}
