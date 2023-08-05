5e031bd86827c62a06f1b9ffc4c28673402fbbcd
MirageM <imiragesz@gmail.com>
2023-08-04 21:47:28 -0400

    foreach(int num in nums)

---
+++ C# Study/Permute.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public IList<IList<int>> Permute(int[] nums) {
+        IList<IList<int>> result = new List<IList<int>>();
+        Backtrack(nums, new List<int>(), result);
+        return result;
+    }
+
+    private void Backtrack(int[] nums, List<int> path, IList<IList<int>> result){
+        if(path.Count == nums.Length){
+            result.Add(new List<int>(path));
+            return;
+        }
+        foreach(int num in nums){
+            if(path.Contains(num)) continue;
+            path.Add(num);
+            Backtrack(nums, path, result);
+            path.RemoveAt(path.Count - 1);
+        }
+    }
+}
