3c143a283889fd195c35085232bc8fe4bf5aa8a2
MirageM <imiragesz@gmail.com>
2023-08-05 22:43:53 -0400

    root.left = BuildTree(inorder[1..(middle+1)], preorder[..middle]);

--- C# Study/BuildTree.cs
+++ C# Study/BuildTree.cs
@@ -13,7 +13,14 @@
  */
 public class Solution {
     public TreeNode BuildTree(int[] preorder, int[] inorder) {
-       
+        if (preorder is null || !preorder.Any() ||
+            inorder is null || !inorder.Any())
+            return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        int middle = Array.IndexOf(inorder, preorder[0]);
+        root.left = BuildTree(preorder[1..(middle+1)], inorder[..middle]);
+        root.right = BuildTree(preorder[(middle+1)..], inorder[(middle+1)..]);
+        return root;
         
     }
 }
