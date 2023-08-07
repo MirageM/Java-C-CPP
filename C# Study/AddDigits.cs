8977ed58a47b523e7868a23ac9c415e9ca97af48
MirageM <imiragesz@gmail.com>
2023-08-06 20:51:18 -0400

    return ans == 0 ? 9 : ans

--- C# Study/AddDigits.cs
+++ C# Study/AddDigits.cs
@@ -1,6 +1,10 @@
 public class Solution {
     public int AddDigits(int num) {
-        
-        
+        if (num == 0)
+        {
+            return 0;
+        }
+        int ans = num % 9;
+        return ans == 0 ? 9: ans;
     }
 }
